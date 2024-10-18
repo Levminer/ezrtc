34451c3734f392e7caf399ee6133d5c2dcac3c57
Lőrik Levente <levminer@levminer.com>
2024-10-18 22:15:53 +0200

    Update axum

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,30 +2,22 @@ use std::env;
 use std::net::SocketAddr;
 use std::str::FromStr;
 
+use ezrtc_server::router::{self, ServerState};
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
-use ezrtc_server::router::{self, ServerState};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    TermLogger::init(
-        LevelFilter::Debug,
-        Config::default(),
-        TerminalMode::Mixed,
-        ColorChoice::Auto,
-    )?;
+    TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed, ColorChoice::Auto)?;
 
     let server_state = ServerState::default();
     let app = router::create(server_state);
 
-    let address = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "0.0.0.0:9001".to_string());
-    let address = SocketAddr::from_str(&address)?;
+    let address = env::args().nth(1).unwrap_or_else(|| "0.0.0.0:9001".to_string());
+    let socket_addr = SocketAddr::from_str(&address)?;
+    let listener = tokio::net::TcpListener::bind(socket_addr).await?;
 
-    axum::Server::bind(&address)
-        .serve(app.into_make_service())
-        .await?;
+    axum::serve::serve(listener, app.into_make_service()).await?;
 
     Ok(())
 }
